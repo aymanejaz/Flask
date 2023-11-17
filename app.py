35059e60dc04985a94bfb932136f84ca11f663d8
aymanijaz <aymanijaz21@gmail.com>
2023-11-17 13:35:52 +0500

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+## Flask App Routing
+
+from flask import Flask
+
+## create a simple flask application
+app = Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def welcome():
+    return "Welcome to my Flask page"
+
+@app.route("/index",methods=["GET"])
+def index():
+    return "Welcome to the Index page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
