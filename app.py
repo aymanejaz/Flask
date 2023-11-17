e18ef77c9de97c8ce5ddc805c93cf113239735fc
aymanijaz <aymanijaz21@gmail.com>
2023-11-18 00:20:46 +0500

    Calculate average marks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ## Flask App Routing
 
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
 
 ## create a simple flask application
 app = Flask(__name__)
@@ -12,6 +12,34 @@ def welcome():
 @app.route("/index",methods=["GET"])
 def index():
     return "Welcome to the Index page"
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "The person has passed and and the score is: "+ str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The person has fail and and the score is: "+ str(score)
+
+@app.route('/form', methods=["GET", "POST"])
+def form():
+    if request.method=="GET":
+        return render_template('form.html')
+    else: 
+        maths = float(request.form['math'])
+        science = float(request.form['science'])
+        english = float(request.form['english'])
+
+        average_marks=(maths+science+english)/3
+
+        # return render_template('form.html',score=average_marks)
+
+        res=""
+        if average_marks>=50:
+            res='success'
+        else:
+            res='fail'
+        return redirect(url_for(res,score=average_marks))
 
 
 if __name__ == "__main__":
